664dbee1c51f4b3584fa2d1a62ff3349e944c687
Isaiah <isaiah.chua@gmail.com>
2023-03-08 18:03:54 +0800

    Added YAML config file with template values, created a ipynb file for running preprocessing, training and testing; created the skeleton for converting the nii files to numpy files

---
+++ niitonumpy.py
@@ -0,0 +1,19 @@
+import sys, os
+import json
+import numpy as np
+import nibabel
+import SimpleITK as sitk
+
+class NiiToNumpy:
+
+    def __init__(self):
+        return
+
+    def convertwithsitk():
+        return
+
+    def convertwithnib():
+        return
+
+if __name__ == "__main__":
+    pass
