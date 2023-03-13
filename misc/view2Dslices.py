bc1f5f7cf8881b2ebd6cec0993a86cfa4f99b741
Isaiah <isaiah.chua@gmail.com>
2023-03-13 12:18:48 +0800

    add some comments to view2Dslice.py so code is more understandable

--- misc/view2Dslices.py
+++ misc/view2Dslices.py
@@ -11,18 +11,23 @@ def RandXYZSlices(ct_file, view_indices=None):
     patient = dirname(ct_file).split("/")[-1]
     try:
         ct_ds = nib.load(ct_file)
+        # transpose image to flip it correct order
         ct_im = ct_ds.get_fdata().T
     except:
         ct_ds = sitk.ReadImage(ct_file)
         ct_im = sitk.GetArrayFromImage(ct_ds)
     if view_indices == None:
+        # when read as numpy arrays the images are ordered as (z, y, x) instead
+        # of (x, y, z)
         z_idx = random.randint(0, ct_im.shape[0])
         y_idx = random.randint(0, ct_im.shape[1])
         x_idx = random.randint(0, ct_im.shape[2])
     else:
         assert isinstance(view_indices, list)
         assert len(view_indices) == 3
+        # input from user should still be in (x, y, z)
         x_idx, y_idx, z_idx = view_indices
+    # slice images with z, y, x ordering
     z_slice = ct_im[z_idx,:,:]
     y_slice = ct_im[:,y_idx,:]
     x_slice = ct_im[:,:,x_idx]
@@ -31,6 +36,7 @@ def RandXYZSlices(ct_file, view_indices=None):
     axs[0].set_title(f"Sagital view (x={str(x_idx)})")
     axs[0].set_xlabel("y")
     axs[0].set_ylabel("z")
+    # plot images with origin at bottom left instead of top left
     axs[0].imshow(x_slice, cmap="bone", origin="lower")
     axs[1].set_title(f"Frontal/Coronal view (y={str(y_idx)})")
     axs[1].set_xlabel("x")
@@ -44,6 +50,8 @@ def RandXYZSlices(ct_file, view_indices=None):
     plt.show()
 
 if __name__ == "__main__":
+    # Input 1: path to ct image
+    # Input 2: slice indices to view (x, y, z)
     ct_path = sys.argv[1]
     try:
         vidx = ast.literal_eval(sys.argv[2])
