ae5a9b8ffe4c677b4d4a2b0a3d6a3c53ab50a72f
Isaiah <isaiah.chua@gmail.com>
2023-03-13 12:29:02 +0800

    removed old csvtojson.py file and moved splits.json to data folder, note that misc folder is supposed to contain scripts that are used for analysis, troubleshooting, etc... but not used in the final model

--- misc/csvtojson.py
+++ misc/csvtojson.py
@@ -1,7 +1,13 @@
 import numpy as np
 import pandas as pd
+import json
 
-file = "/home/datasets/Total_Segmentor/meta.csv"
+file = "/home/dataset/TotalSegmentor/meta.csv"
 data = pd.read_csv(file, sep=";")
-splits = data.groupby("split")["image_id"]
-print(splits.head())
+final_dict = {}
+for label in list(data["split"].unique()):
+    final_dict[label] = data.loc[data["split"].isin([label]), "image_id"].tolist()
+for key, val in final_dict.items():
+    print(f"{key}: {len(val)}")
+with open("/home/isaiah/TotalSegmentatorProj/splits.json", "w") as f:
+    json.dump(final_dict, f)
